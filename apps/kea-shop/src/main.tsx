8f35d03a51f101579b177a5c22da01600e2be6a3
Rebecca Lang <rebeccalang50@gmail.com>
2024-10-25 17:29:38 +1300

    remove comments

--- apps/kea-shop/src/main.tsx
+++ apps/kea-shop/src/main.tsx
@@ -20,14 +20,3 @@ document.addEventListener('DOMContentLoaded', () => {
     </StrictMode>
   );
 });
-
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
-// root.render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </StrictMode>
-// );
