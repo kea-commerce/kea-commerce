1de79b203b1891d28eef3badfa12289e18aeb604
Rebecca Lang <rebeccalang50@gmail.com>
2024-10-27 15:15:22 +1300

    persister for browser

--- apps/kea-shop/src/main.tsx
+++ apps/kea-shop/src/main.tsx
@@ -5,7 +5,14 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+
 import routes from './app/routes';
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
 
 const router = createBrowserRouter(routes);
 const queryClient = new QueryClient();
@@ -13,10 +20,13 @@ const queryClient = new QueryClient();
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <StrictMode>
-      <QueryClientProvider client={queryClient}>
+      <PersistQueryClientProvider
+        client={queryClient}
+        persistOptions={{ persister }}
+      >
         <RouterProvider router={router} />
         <ReactQueryDevtools />
-      </QueryClientProvider>
+      </PersistQueryClientProvider>
     </StrictMode>
   );
 });
