df774c80814a652e7d5b4108ce64e975c60aa37c
olivia-burgess <oliviaburgess@protonmail.com>
2024-11-04 17:40:56 +1300

    style nav bar

--- libs/shared/src/lib/nav.tsx
+++ libs/shared/src/lib/nav.tsx
@@ -1,37 +1,53 @@
 import { IoSearch } from 'react-icons/io5';
 import { MdOutlineAccountCircle, MdOutlineShoppingCart } from 'react-icons/md';
+import { FaChevronDown } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 
 export function Nav() {
   const iconClass = 'border-2 rounded desktop:p-0.5 desktop:w-12 desktop:h-12';
 
   return (
-    <div className="bg-lightYellow flex">
+    <div className="bg-lightYellow flex items-center justify-between">
       <img
         src="/mainLogo.png"
         alt="E-Kea Comm logo"
         className="desktop:max-w-52"
       />
+      <div className="flex items-center justify-between gap-4 pr-3">
+        
+        <div className="flex items-center justify-between">
+        <Link to="/about">
+          <p className='desktop:text-2xl'>OUR STORY</p>
+          </Link>
+          <FaChevronDown className='desktop:w-8'/>
+          </div>
+       
+        
+        <div className="flex items-center justify-between">
+        <Link to="/collections/:collection">
+          <p className='desktop:text-2xl'>SHOP</p>
+          </Link>
+          <FaChevronDown className='desktop:w-8'/>
+          
+          </div>
+       
 
-      <Link to="/about">
-        <p>Our Story</p>
-      </Link>
+        <div className="flex items-center justify-between gap-2 pr-3">
+          <Link to="/search">
+            <IoSearch className={iconClass} />
+          </Link>
 
-      <Link to="/collections/:collection">
-        <p>Shop</p>
-      </Link>
+          <Link to="/cart">
+            <MdOutlineShoppingCart className={iconClass} />
+          </Link>
 
-      <Link to="/cart">
-        <MdOutlineShoppingCart className={iconClass} />
-      </Link>
+          <Link to="/profile/:name">
+            <MdOutlineAccountCircle className={iconClass} />
+          </Link>
+        </div>
+      </div>
 
-      <Link to="/profile/:name">
-        <MdOutlineAccountCircle className={iconClass} />
-      </Link>
-
-      <Link to="/search">
-        <IoSearch className={iconClass} />
-      </Link>
+      
     </div>
   );
 }
