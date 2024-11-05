9a537e716662e42229a0f2ff329d9235dce85131
Rebecca Lang <rebeccalang50@gmail.com>
2024-11-06 12:33:52 +1300

    added shop dropdown functionality, icons and links.

--- libs/shared/src/lib/nav-links.tsx
+++ libs/shared/src/lib/nav-links.tsx
@@ -1,28 +1,44 @@
-import { FaChevronDown } from 'react-icons/fa6';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { ShopDropdown } from './shop-dropdown';
 
 export const NavLinks = () => {
+  const [shopOpen, setShopOpen] = useState(false);
+
+  const toggleShop = () => {
+    setShopOpen(!shopOpen);
+  };
   const navLinks = [
-    { title: 'OUR STORY', url: '/about' },
     {
       title: 'SHOP',
       url: '/collections/:collection',
-      icon: <FaChevronDown className="w-8" />,
+      icon: {
+        iconDown: <FaChevronDown className="w-8" />,
+        iconUp: <FaChevronUp className="w-8" />,
+      },
     },
+    { title: 'OUR STORY', url: '/about' },
+
     { title: 'ACCOUNT', url: '/profile/:name' },
   ];
 
   return (
-    <div className="gap-5 bg-white bg-opacity-50 h-screen rounded-sm">
-      <ul className="gap-4">
+    <div className="gap-5 bg-white bg-opacity-50 rounded">
+      <ul className="gap-4 py-5">
         {navLinks.map((link, index) => (
           <Link to={link.url}>
-            <div className="flex items-center">
-              <li key={index} className="text-2xl pl-4 p-3">
+            <div className="flex items-center pb-3">
+              <li key={index} className="text-2xl pl-4">
                 {link.title}
               </li>
-              {link.icon && <span>{link.icon}</span>}
+              {link.icon && (
+                <button onClick={toggleShop}>
+                  {shopOpen ? link.icon.iconUp : link.icon.iconDown}
+                </button>
+              )}
             </div>
+            {link.icon && shopOpen && <ShopDropdown />}
           </Link>
         ))}
       </ul>
---
+++ libs/shared/src/lib/shop-dropdown.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+const collections = [
+  { title: 'Bath & Body', link: 'bath-body' },
+  { title: 'Home', link: 'home' },
+  { title: 'Kids & Baby', link: 'kids-baby' },
+  { title: 'Kitchen', link: 'kitchen' },
+  { title: 'Laundry & Cleaning', link: 'laundry-cleaning' },
+  { title: 'Pets', link: 'pets' },
+  { title: 'Shop All Products', link: 'shop' },
+];
+
+export const ShopDropdown = () => {
+  return (
+    <ul className="pl-7 pb-4">
+      {collections.map((category, index) => (
+        <Link
+          to={`collections/${category.link}`}
+          aria-label={`shop for ${category.title}`}
+        >
+          <li key={index}>{category.title}</li>
+        </Link>
+      ))}
+    </ul>
+  );
+};
