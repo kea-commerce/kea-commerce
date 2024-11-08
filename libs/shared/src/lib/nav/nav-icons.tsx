8db5358db5ddb204dd2ec56a04ace44f133d84bb
Rebecca Lang <rebeccalang50@gmail.com>
2024-11-08 17:29:39 +1300

    add border to icons, created types file, imported types into NavIcons

--- libs/shared/src/lib/nav/nav-icons.tsx
+++ libs/shared/src/lib/nav/nav-icons.tsx
@@ -3,17 +3,12 @@ import { IoSearch } from 'react-icons/io5';
 import { MdOutlineAccountCircle, MdOutlineShoppingCart } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
-type Icons = {
-  icon: JSX.Element;
-  url: string;
-  label: string;
-  showAlways?: boolean;
-  desktopOnly?: boolean;
-};
+import type { LinkIcon } from './type';
 
 export const NavIcons = () => {
-  const iconClass = 'border rounded p-0.5 w-8 h-8 md:p-1';
-  const iconList: Icons[] = [
+  const iconClass = 'border-2 border-black rounded-full p-0.5 w-8 h-8 md:p-1';
+
+  const iconList: LinkIcon[] = [
     {
       icon: <IoSearch className={iconClass} />,
       url: '/search',
@@ -40,7 +35,7 @@ export const NavIcons = () => {
     <ul className='flex items-center gap-2 md:p-2 md:gap-3'>
       {filteredNavIcons.map((icon) => (
         <Link aria-label={icon.label} key={icon.label} to={icon.url}>
-          <li>{icon.icon}</li>
+          <li key={icon.label}>{icon.icon}</li>
         </Link>
       ))}
     </ul>
---
+++ libs/shared/src/lib/nav/type.ts
@@ -0,0 +1,7 @@
+export type LinkIcon = {
+  icon: JSX.Element;
+  url: string;
+  label: string;
+  showAlways?: boolean;
+  desktopOnly?: boolean;
+};
