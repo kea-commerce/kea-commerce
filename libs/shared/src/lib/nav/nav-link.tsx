81d7f67fb9df1eaedc2af64ebbf47bc57f9c7c49
Rebecca Lang <rebeccalang50@gmail.com>
2024-11-08 17:51:52 +1300

    add arialabel, useCallback and changed props

--- libs/shared/src/lib/nav/nav-link.tsx
+++ libs/shared/src/lib/nav/nav-link.tsx
@@ -1,27 +1,27 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import type { LinkIcons, Links } from './nav-links';
 import { ShopDropdown } from './shop-dropdown';
 
-type Props = {
+type NavLinkProps = Readonly<{
   links: Links;
   icons?: LinkIcons;
-};
+}>;
 
-export const NavLink = ({ links, icons }: Props) => {
+export const NavLink = ({ links, icons }: NavLinkProps) => {
   const [shopOpen, setShopOpen] = useState(false);
 
-  const toggleShop = () => {
+  const toggleShop = useCallback(() => {
     setShopOpen(!shopOpen);
-  };
+  }, [shopOpen]);
 
   return (
     <Link key={links.title} to={links.url}>
       <div className='flex items-center pb-3 md:p-0'>
         <li className='text-2xl pl-4 md:p-0'>{links.title}</li>
         {icons ? (
-          <button onClick={toggleShop} type='button'>
+          <button aria-label={`${shopOpen ? 'Close' : 'Open'} shop menu`} onClick={toggleShop} type='button'>
             {shopOpen ? icons.iconUp : icons.iconDown}
           </button>
         ) : undefined}
