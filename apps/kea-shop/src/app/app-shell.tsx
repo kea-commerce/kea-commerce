187c72789de327ad30f052b25116e7ed5f2543c4
Shae Jacobs <shae.jacobs01@gmail.com>
2024-11-12 11:57:22 +1300

    split auth0 provider and QueryClientProvider into app-shell.tsx

---
+++ apps/kea-shop/src/app/app-shell.tsx
@@ -0,0 +1,32 @@
+import { useMemo } from 'react';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+
+import { type CustomWindow } from '@kea-commerce/shared/models';
+
+declare const window: CustomWindow;
+
+const queryClient = new QueryClient();
+
+const persister = {
+  persister: createSyncStoragePersister({
+    storage: window.localStorage,
+  }),
+};
+
+const authParams = { redirect_uri: globalThis.location.origin, audience: 'https://keacommerce/api' };
+
+export const AppShell = () => {
+  return (
+    <>
+      <Auth0Provider
+        authorizationParams={authParams}
+        clientId='oMrrnFgzFSoONc03BQNshhpVJ7TSbU8p'
+        domain='kotare-2024-shae.au.auth0.com'
+      />
+      <PersistQueryClientProvider client={queryClient} persistOptions={persister} />
+    </>
+  );
+};
--- apps/kea-shop/src/main.tsx
+++ apps/kea-shop/src/main.tsx
@@ -1,40 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
-import { QueryClient } from '@tanstack/react-query';
+
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 
-import type { CustomWindow } from '@kea-commerce/shared/models';
-
+import { AppShell } from './app/app-shell';
 import { routes } from './app/routes';
-
-declare const window: CustomWindow;
-
-const queryClient = new QueryClient();
-
-const persister = createSyncStoragePersister({
-  storage: window.localStorage,
-});
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.querySelector('#root') as HTMLElement).render(
   <StrictMode>
-    <Auth0Provider
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-        audience: 'https://keacommerce/api',
-      }}
-      clientId='oMrrnFgzFSoONc03BQNshhpVJ7TSbU8p'
-      domain='kotare-2024-shae.au.auth0.com'
-    >
-      <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
-        <RouterProvider router={router} />
+    <AppShell >
+            <RouterProvider router={router} />
         <ReactQueryDevtools />
-      </PersistQueryClientProvider>
-    </Auth0Provider>
+
+    </AppShell>
   </StrictMode>
 );
