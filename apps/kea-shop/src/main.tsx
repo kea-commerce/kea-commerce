00c93a94b4ee15d74101f02d0dd340689f5bc3f6
bosh-code <ryan@bosher.co.nz>
2024-11-12 16:27:31 +1300

    update app-shell to contain providers

--- apps/kea-shop/src/main.tsx
+++ apps/kea-shop/src/main.tsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 
 import { AppShell } from './app/app-shell';
-import { routes } from './app/routes';
-
-const router = createBrowserRouter(routes);
 
 createRoot(document.querySelector('#root') as HTMLElement).render(
   <StrictMode>
-    <AppShell >
-            <RouterProvider router={router} />
-        <ReactQueryDevtools />
-
-    </AppShell>
+    <AppShell />
   </StrictMode>
 );
