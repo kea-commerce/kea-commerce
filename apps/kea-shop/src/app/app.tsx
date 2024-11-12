5847e8e851dc2175f4aaa7350ab2c5e5b985cbce
olivia-burgess <oliviaburgess@protonmail.com>
2024-11-12 15:02:17 +1300

    add initial form with styling. apply font globally. add outlet

--- apps/kea-shop/src/app/app.tsx
+++ apps/kea-shop/src/app/app.tsx
@@ -1,15 +1,13 @@
+import { Outlet } from 'react-router';
+
 import { Footer } from '@kea-commerce/shared/footer';
 import { Nav } from '@kea-commerce/shared/nav';
-
-import NxWelcome from './nx-welcome';
 
 export const App = () => {
   return (
     <div>
       <Nav />
-
-      <NxWelcome title='kea-shop' />
-
+      <Outlet />
       <Footer />
     </div>
   );
--- apps/kea-shop/src/pages/contact.tsx
+++ apps/kea-shop/src/pages/contact.tsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
 const Contact = () => {
-  return <div>Contact</div>;
+
+  const formData = {
+    name: '',
+    email: '',
+    message: ''
+  }
+
+  const [formState, setFormState] = useState(formData)
+
+  return (
+  <div className="flex justify-center gap-10">
+  <div className="text-2xl">Contact Us</div>
+
+<div>
+  <form className="flex flex-col">
+    <label htmlFor='name'>Name</label>
+    <input id='name' name='name' type='text' className="border"/>
+  
+
+    <label htmlFor='email'>Email</label>
+    <input id='email' name='email' type='text' className="border" />
+    
+
+    <label htmlFor='name'>Message</label>
+    <textarea id='name' name='name' className="border" />
+    
+    <button type="button" className="border rounded mt-2 mb-2">Submit</button>
+
+  </form>
+  </div>
+  </div>
+)
 };
 
 export default Contact;
+
