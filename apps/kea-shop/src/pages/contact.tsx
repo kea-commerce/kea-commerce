65f29f6b2d4b85b156bce3c6cb55289fe1259236
olivia-burgess <oliviaburgess@protonmail.com>
2024-11-13 15:42:12 +1300

    remove items that got split to own compoent

--- apps/kea-shop/src/pages/contact.tsx
+++ apps/kea-shop/src/pages/contact.tsx
@@ -1,53 +1,15 @@
-<<<<<<< HEAD
-import { type FormEvent, useCallback, useState } from "react";
-import { useNavigate } from "react-router";
+import { Form } from '@kea-commerce/shared/form';
 
-const Contact = () => {
-
-  const formData = {
-    name: '',
-    email: '',
-    message: ''
-  }
-
-  const [formState, setFormState] = useState(formData)
-
-  const navigate = useNavigate()
-
-  const handleSubmit = useCallback(async (evt: FormEvent) => {
-    await submitNewContactReq.mutate({formState})
-    navigate('/contact-submitted')
-  }, [formData])
+export const Contact = () => {
 
   return (
-  <div className="flex justify-center gap-10">
-  <div className="text-2xl">Contact Us</div>
-
-<div>
-  <form className="flex flex-col">
-    <label htmlFor='name'>Name</label>
-    <input id='name' name='name' type='text' required className="border"/>
-  
-
-    <label htmlFor='email'>Email</label>
-    <input id='email' name='email' type='text' required className="border" />
-    
-
-    <label htmlFor='name'>Message</label>
-    <textarea id='name' name='name' required className="border" />
-    
-    <button onClick={handleSubmit} type="button" className="border rounded mt-2 mb-2">Submit</button>
-
-  </form>
-  </div>
+    <div>
+      <div className="flex justify-center gap-10">
+      <div className="text-2xl">Contact Us</div>
+      <div className="bg-slate-50">
+        <Form />
+      </div>
+      </div>
   </div>
 )
-};
-
-export default Contact;
-
-=======
-export const Contact = () => {
-  return <div>Contact</div>;
-};
->>>>>>> main
+}
