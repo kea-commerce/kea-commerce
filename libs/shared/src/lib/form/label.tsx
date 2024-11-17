3225b51aebcf3a8f693299c4c754f9b451302f21
olivia-burgess <oliviaburgess@protonmail.com>
2024-11-18 12:07:00 +1300

    refactor label and add some additional content

--- libs/shared/src/lib/form/label.tsx
+++ libs/shared/src/lib/form/label.tsx
@@ -1,21 +1,44 @@
+import type { ChangeEvent } from 'react';
+
 type LabelProps = Readonly<{
   name: string;
+  label: string;
   type: string;
+  value: string;
+  placeholder: string;
+  handleChange: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 }>;
 
-export const Label = ({ name, type }: LabelProps) => {
+//add something that when required fielld not filled it will outlien that red and have error message for the user e.g. please enter your name
+
+export const Label = ({ name, type, value, placeholder, label, handleChange }: LabelProps) => {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col my-2'>
       <label className='focus:border-slate-50' htmlFor={name}>
-        {name}
+        {label}
       </label>
-      <input
-        className='border hover:border-slate-800 focus:border-slate-800 focus:outline-none'
-        id={name}
-        name={name}
-        required
-        type={type}
-      />
+      {type === 'text' ? (
+        <input
+          className='border border-lightGrey focus:border-keaGreen focus:outline-none px-4 py-2 rounded-md'
+          id={name}
+          name={name}
+          onChange={handleChange}
+          placeholder={value === '' ? placeholder : value}
+          required
+          type={type}
+          value={value}
+        />
+      ) : (
+        <textarea
+          className='border border-lightGrey focus:border-keaGreen focus:outline-none px-4 py-2 rounded-md'
+          id={name}
+          name={name}
+          onChange={handleChange}
+          placeholder={value === '' ? placeholder : value}
+          required
+          value={value}
+        />
+      )}
     </div>
   );
 };
