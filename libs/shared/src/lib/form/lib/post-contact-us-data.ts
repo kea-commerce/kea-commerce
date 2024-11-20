690129cfdc0fc08911ed6bff3420781d705a66ba
olivia-burgess <oliviaburgess@protonmail.com>
2024-11-20 15:04:57 +1300

    fix issues raised on pr

--- libs/shared/src/lib/form/lib/post-contact-us-data.ts
+++ libs/shared/src/lib/form/lib/post-contact-us-data.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { type ContactUs } from '@kea-commerce/shared/models';
+import { type ContactUs } from '../../models';
 
 export const postContactUs = async (formData: ContactUs): Promise<void> => {
   await axios.post<ContactUs>('http://localhost:3333/api/contact-us', formData);
