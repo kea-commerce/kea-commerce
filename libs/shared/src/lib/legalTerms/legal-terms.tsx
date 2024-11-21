85e9c054fb0254f98d4e9c57e4e8641d5a9554ab
Shae Jacobs <shae.jacobs01@gmail.com>
2024-11-22 09:06:39 +1300

    working on tailwind and making drop down for sections

--- libs/shared/src/lib/legalTerms/legal-terms.tsx
+++ libs/shared/src/lib/legalTerms/legal-terms.tsx
@@ -1,17 +1,36 @@
+import { useCallback, useState } from 'react';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa6';
+
 import { TermsAndConditions } from './terms-and-conditions';
 
 export const LegalTerms = () => {
+  // State to track open/close status for each section
+  const [openSections, setOpenSections] = useState<Record<number, boolean>>({});
+
+  const toggleSection = (sectionNumber: number) => {
+    setOpenSections((prevState) => ({
+      ...prevState,
+      [sectionNumber]: !prevState[sectionNumber],
+    }));
+  };
+
   return (
-    <div>
-      <div>
+    <div className=''>
+      <div className='justify-items-center w-full text-3xl p-10'>
         <h1>{TermsAndConditions.title}</h1>
       </div>
-      <div>
+      <div className='p-10 pt-0'>
         {TermsAndConditions.sections.map((section) => (
           <div key={section.sectionNumber}>
-            <h2>{`${section.sectionNumber}. ${section.heading}`}</h2>
+            <button onClick={() => toggleSection(section.sectionNumber)} type='button'>
+              <h2 className='font-bold text-2xl'>{`${section.sectionNumber}. ${section.heading}`}</h2>
+              {openSections[section.sectionNumber] ? (
+                <FaChevronDown className='w-8' />
+              ) : (
+                <FaChevronUp className='w-8' />
+              )}
+            </button>
             <p>{section.information}</p>
-
             {section.subSections && section.subSections.length > 0 ? (
               <div>
                 {section.subSections.map((subSection) => (
