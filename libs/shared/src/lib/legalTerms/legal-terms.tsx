e4d90fa1316214b3dbddb3a5bb1436875ec1610a
Shae <shae.jacobs01@gmail.com>
2024-11-28 12:02:38 +1300

    Update libs/shared/src/lib/legalTerms/legal-terms.tsx
    Co-authored-by: Ryan Bosher <ryan@bosher.co.nz>

--- libs/shared/src/lib/legalTerms/legal-terms.tsx
+++ libs/shared/src/lib/legalTerms/legal-terms.tsx
@@ -7,6 +7,7 @@ import { type LegalTermsLayout } from './type';
 type LegalTermsContentProps = Readonly<{
   content: LegalTermsLayout;
 }>;
+
 export const LegalTerms = ({ content }: LegalTermsContentProps) => {
   return (
     <div className='justify-items-center'>
