7b09ec04d69aa863c3b2e1d457c0042c88582575
Shae Jacobs <shae.jacobs01@gmail.com>
2024-11-28 12:26:42 +1300

    removed unused type

--- libs/shared/src/lib/legalTerms/type.ts
+++ libs/shared/src/lib/legalTerms/type.ts
@@ -15,17 +15,3 @@ export type LegalTermsLayout = {
   title: string;
   sections: Section[];
 };
-
-export type TermsDropDown = {
-  title: string;
-  sections: {
-    sectionNumber: number;
-    heading: string;
-    information: string;
-    subSections?: {
-      subSectionNumber: string;
-      subHeading: string;
-      subInformation: string;
-    }[];
-  }[];
-};
