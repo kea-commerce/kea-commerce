ee32a8a591212b24569cb3274f67c1b0e6c0c036
Shae <shae.jacobs01@gmail.com>
2024-11-28 12:02:53 +1300

    Update libs/shared/src/lib/legalTerms/legal-terms.tsx
    Co-authored-by: Ryan Bosher <ryan@bosher.co.nz>

--- libs/shared/src/lib/legalTerms/legal-terms.tsx
+++ libs/shared/src/lib/legalTerms/legal-terms.tsx
@@ -8,18 +8,17 @@ type LegalTermsContentProps = Readonly<{
   content: LegalTermsLayout;
 }>;
 
-export const LegalTerms = ({ content }: LegalTermsContentProps) => {
-  return (
-    <div className='justify-items-center'>
-      <div className='justify-items-center w-full text-3xl p-10'>
-        <h1>{content.title}</h1>
-      </div>
-      <div className='flex flex-row'>
-        <div className='md: px-10'>{isMobile ? undefined : <LegalTermsNavbar navbarContent={content} />}</div>
-        <div className='md:p-10 md:pt-0 pb-10'>
-          <LegalTermsContent content={content} />
-        </div>
+export const LegalTerms = ({ content }: LegalTermsContentProps) => (
+  <div className='justify-items-center'>
+    <div className='justify-items-center w-full text-3xl p-10'>
+      <h1>{content.title}</h1>
+    </div>
+    <div className='flex flex-row'>
+      <div className='md: px-10'>{isMobile ? undefined : <LegalTermsNavbar navbarContent={content} />}</div>
+      <div className='md:p-10 md:pt-0 pb-10'>
+        <LegalTermsContent content={content} />
       </div>
     </div>
-  );
-};
+  </div>
+);
+
