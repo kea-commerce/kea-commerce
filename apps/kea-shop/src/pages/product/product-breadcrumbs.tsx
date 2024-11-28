3678062c442be86d259418a5f998d0e1be2bbf3d
Rebecca Lang <rebeccalang50@gmail.com>
2024-11-28 16:03:40 +1300

    styling product breadcrumb, home link removed from product breadcrumb

--- apps/kea-shop/src/pages/product/product-breadcrumbs.tsx
+++ apps/kea-shop/src/pages/product/product-breadcrumbs.tsx
@@ -1,3 +1,4 @@
+import { RxSlash } from 'react-icons/rx';
 import { Link } from 'react-router-dom';
 
 type ProductBreadcrumbProps = Readonly<{
@@ -8,14 +9,16 @@ type ProductBreadcrumbProps = Readonly<{
 
 export const ProductBreadcrumb = ({ collectionName, collectionLink, productName }: ProductBreadcrumbProps) => {
   return (
-    <div>
-      <div>
-        <Link to='/'>Home</Link>
-      </div>
-      <div>
-        <Link to={`/collections/${collectionLink}`}>{collectionName}</Link>
-      </div>
-      <div>{productName}</div>
+    <div className='text-lg flex flex-row items-center md:gap-2 md:text-xl'>
+      <Link className='hover:font-medium' to='/collections/shop'>
+        Shop
+      </Link>
+      <RxSlash className='pt-1' />
+      <Link className='hover:font-medium' to={`/collections/${collectionLink}`}>
+        {collectionName}
+      </Link>
+      <RxSlash className='pt-1' />
+      <h2 className='font-normal'>{productName}</h2>
     </div>
   );
 };
--- apps/kea-shop/src/pages/product/product-page.tsx
+++ apps/kea-shop/src/pages/product/product-page.tsx
@@ -22,33 +22,37 @@ export const ProductPage = () => {
   const collectionBreadcrumb = collections.find((collection) => collection.title === data.category);
 
   return (
-    <div className='flex items-center justify-center p-4 pt-12 md:bg-lighterYellow'>
-      <ProductBreadcrumb
-        collectionLink={collectionBreadcrumb?.link}
-        collectionName={data.category}
-        productName={data.name}
-      />
-      <div className='flex flex-col md:flex-row md:gap-8 md:items-center md:justify-center'>
-        <div className='md:w-1/2 lg:w-2/5'>
-          <img
-            alt={`product of ${data.name}`}
-            className='w-full border-2 border-slate-200 rounded-2xl md:w-96'
-            src={data.productImage}
+    <div className='md:bg-lighterYellow md:flex md:flex-row md:items-center md:justify-center'>
+      <div className='flex flex-col justify-center p-4'>
+        <div className='pl-2 pb-3 pr-5 md:flex md:justify-self-start md:py-3 md:text-xl'>
+          <ProductBreadcrumb
+            collectionLink={collectionBreadcrumb?.link}
+            collectionName={data.category}
+            productName={data.name}
           />
         </div>
-        <div className='flex flex-col md:w-1/2 lg:w-3/5 space-y-6'>
-          <div className='space-y-2'>
-            <h2 className='font-medium text-3xl pt-6 md:pt-0 md:text-4xl lg:text-6xl'>{data.name}</h2>
-            <p className='font-medium text-xl md:text-2xl lg:text-4xl'>${data.price}</p>
+        <div className='flex flex-col md:flex-row md:gap-8 md:items-center md:justify-center'>
+          <div className='md:w-1/2 lg:w-2/5'>
+            <img
+              alt={`product of ${data.name}`}
+              className='w-full border-2 border-slate-200 rounded-2xl md:w-96'
+              src={data.productImage}
+            />
           </div>
-          <div className='flex flex-row items-center gap-10 md:text-xl lg:text-2xl'>
-            Quantity:
-            <Quantity stock={data.stock} />
-            <AddToCartButton />
-          </div>
-          <div className='space-y-2'>
-            <h3 className='font-semibold text-xl md:text-2xl lg:text-3xl'>Description</h3>
-            <p className='md:text-lg lg:text-xl'>{data.description}</p>
+          <div className='flex flex-col md:w-1/2 lg:w-3/5 space-y-6'>
+            <div className='space-y-2'>
+              <h2 className='font-medium text-3xl pt-6 md:pt-0 md:text-4xl lg:text-6xl'>{data.name}</h2>
+              <p className='font-medium text-xl md:text-2xl lg:text-4xl'>${data.price}</p>
+            </div>
+            <div className='flex flex-row items-center gap-10 md:text-xl lg:text-2xl'>
+              Quantity:
+              <Quantity stock={data.stock} />
+              <AddToCartButton />
+            </div>
+            <div className='space-y-2'>
+              <h3 className='font-semibold text-xl md:text-2xl lg:text-3xl'>Description</h3>
+              <p className='md:text-lg lg:text-xl'>{data.description}</p>
+            </div>
           </div>
         </div>
       </div>
