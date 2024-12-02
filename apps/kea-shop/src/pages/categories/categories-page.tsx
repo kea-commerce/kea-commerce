b06b56de238648bb8d99c0762c72f2b88c5f6ab1
Rebecca Lang <rebeccalang50@gmail.com>
2024-12-02 15:08:44 +1300

    moved components for styling
    Co-authored-by: Shae <Shae-Jacobs@users.noreply.github.com>

--- apps/kea-shop/src/pages/categories/categories-page.tsx
+++ apps/kea-shop/src/pages/categories/categories-page.tsx
@@ -1,14 +1,26 @@
 import { isMobile } from 'react-device-detect';
+import { useParams } from 'react-router';
 
 import { Collections } from './collections';
 import { FilteredProducts } from './filtered-products';
+import { useAllProductsData } from './lib';
 
 export const CategoriesPage = () => {
+  const { data: allProducts, isError, error, isPending } = useAllProductsData();
+
+  if (isPending || !allProducts) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
   return (
-    <div className='flex w-full h-fit'>
-      <div className='flex-none sticky top-0 h-screen'>{isMobile ? undefined : <Collections />}</div>
-      <div className='flex-1 flex-row justify-center h-fit px-4 pb-10'>
-        <FilteredProducts />
+    <div className='md:flex md:w-full md:h-fit'>
+      <div className='flex-none sticky top-0 md:h-screen'>{isMobile ? undefined : <Collections />}</div>
+      <div className='w-full'>
+        <FilteredProducts products={allProducts.data} />
       </div>
     </div>
   );
--- apps/kea-shop/src/pages/categories/filtered-products.tsx
+++ apps/kea-shop/src/pages/categories/filtered-products.tsx
@@ -6,16 +6,19 @@ import { collections } from '@kea-commerce/shared/collections-types';
 import { type Product } from '@kea-commerce/shared/models';
 import { ProductCard } from '@kea-commerce/shared/product';
 
-import { sortingMethods } from './lib/sorting-methods';
-import { useAllProductsData } from './lib/use-all-products-data';
 import { CategoriesBreadcrumb } from './categories-breadcrumb';
+import { sortingMethods } from './lib';
 
-export const FilteredProducts = () => {
-  const { collection } = useParams<{ collection: string }>();
-  const { data: allProducts, isError, error, isPending } = useAllProductsData();
+type FilteredProductsProps = Readonly<{
+  products: Product[];
+}>;
 
+export const FilteredProducts = ({ products }: FilteredProductsProps) => {
   const [sortMethod, setSortMethod] = useState('featured');
   const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+  const { collection } = useParams<{ collection: string }>();
+
+  const collectionNames = collections.find((collectionModel) => collectionModel.link === collection);
 
   const handleChangeFilter = useCallback(
     (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -24,59 +27,48 @@ export const FilteredProducts = () => {
       const sortFunction = sortingMethods[selectedMethod as keyof typeof sortingMethods];
 
       const productsToSort: Product[] =
-        collection === 'shop'
-          ? allProducts?.data || []
-          : allProducts?.data.filter((product) => product.category === collection) || [];
+        collection === 'shop' ? products : products.filter((product) => product.category === collection);
 
       const sortedProducts = sortFunction ? sortFunction(productsToSort) : productsToSort;
 
       setSortMethod(selectedMethod);
       setFilteredProducts(sortedProducts);
     },
-    [allProducts?.data, collection]
+    [collection, products, setFilteredProducts]
   );
 
   useEffect(() => {
-    if (allProducts?.data) {
+    if (products) {
       const initialProducts =
-        collection === 'shop'
-          ? allProducts?.data
-          : allProducts?.data.filter((product) => product.category === collection);
+        collection === 'shop' ? products : products.filter((product) => product.category === collection);
 
       const featuredProducts = [...initialProducts].sort((a, b) => b.stock - a.stock);
       setFilteredProducts(featuredProducts);
     }
-  }, [allProducts, collection]);
-
-  if (isPending) {
-    return <span>Loading...</span>;
-  }
-
-  if (isError) {
-    return <span>Error: {error.message}</span>;
-  }
-  const collectionNames = collections.find((collectionModel) => collectionModel.link === collection);
+  }, [products, collection]);
 
   return (
     <>
-      <div className={isMobile ? 'sticky top-0 bg-white py-5 bg-opacity-90' : 'py-5'}>
-        <CategoriesBreadcrumb collectionName={collectionNames?.title} />
-      </div>
-      <div>
-        <label>
-          Filter Products By:
-          <select name='filterProductsBy' onChange={handleChangeFilter} value={sortMethod}>
-            <option value='featured'>Featured</option>
-            <option value='bestSelling'>Best Selling</option>
-            <option value='highToLowPrice'>Price High to Low</option>
-            <option value='lowToHighPrice'>Price Low to High</option>
-            <option value='alphabeticallyFromA'>Alphabetically from A-Z</option>
-            <option value='alphabeticallyFromZ'>Alphabetically from Z-A</option>
-          </select>
-        </label>
+      <div className='flex justify-between items-center'>
+        <div className={isMobile ? 'sticky top-0 bg-white py-3 bg-opacity-90' : 'py-5'}>
+          <CategoriesBreadcrumb collectionName={collectionNames?.title} />
+        </div>
+        <div className={isMobile ? 'px-5 pb-3 text-lg' : 'text-xl'}>
+          <label>
+            Filter Products:
+            <select className='border-2 ml-2' name='filterProductsBy' onChange={handleChangeFilter} value={sortMethod}>
+              <option value='featured'>Featured</option>
+              <option value='bestSelling'>Best Selling</option>
+              <option value='highToLowPrice'>Price High to Low</option>
+              <option value='lowToHighPrice'>Price Low to High</option>
+              <option value='alphabeticallyFromA'>Alphabetically from A-Z</option>
+              <option value='alphabeticallyFromZ'>Alphabetically from Z-A</option>
+            </select>
+          </label>
+        </div>
       </div>
 
-      <div className='grid grid-cols-2 justify-center gap-5 md:grid-cols-4'>
+      <div className='grid grid-cols-1 justify-center gap-5 md:grid-cols-4'>
         {filteredProducts?.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
