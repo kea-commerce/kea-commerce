cd55d4b1ca7b6bdeb071b38dcd835f434fc45251
bosh-code <ryan@bosher.co.nz>
2024-12-03 23:43:11 +1300

    remove unused file

--- libs/db/src/lib/services/products-service.ts
+++
@@ -1,7 +0,0 @@
-import knex from 'knex';
-
-import { type Product } from '@kea-commerce/shared/models';
-
-export const getAllProducts = async (): Promise<Product[]> => {
-  return knex<Product>('products').select('*');
-};
